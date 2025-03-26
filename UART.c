7379430fa176c2ee8f33da007846222fbf668e06
AlanBugarin <bugarin.alan@uabc.edu.mx>
2025-03-26 14:29:19 -0700

    esqueleto de funciones

--- UART.c
+++ UART.c
@@ -21,3 +21,42 @@ UART_gotoxy(uint8_t com, uint8_t x, uint8_t y);
 // Utils
 void itoa(uint16_t number, char* str, uint8_t base);
 uint16_t atoi(char *str);
+
+void UART_puts(uint8_t com, char *str){
+
+}
+
+void UART_putchar(uint8_t com, char data){
+
+}
+
+uint8_t UART_available(uint8_t com){
+
+}
+
+char UART_getchar(uint8_t com){
+
+}
+
+void UART_gets(uint8_t com, char *str){
+
+}
+
+UART_clrscr(uint8_t com){
+
+}
+
+UART_setColor(uint8_t com, uint8_t color){
+
+}
+
+UART_gotoxy(uint8_t com, uint8_t x, uint8_t y){
+
+}
+
+void itoa(uint16_t number, char* str, uint8_t base){
+
+}
+uint16_t atoi(char *str){
+
+}
